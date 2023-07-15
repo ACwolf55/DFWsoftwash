6840ab6518383a53d8c58cec28b7566e25a03bf5
logan kelty <lolokelty2000@gmail.com>
2023-07-15 17:57:29 -0500

    first commit

---
+++ src/components/HeroImg.js
@@ -0,0 +1,27 @@
+import "./HeroImgStyles.css";
+import React from "react";
+import IntroImg from "../assets/intro-bg.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+return (
+<div className="hero"> 
+    <div className="mask">
+    <img className="into-img" src=
+    {IntroImg} alt="IntroImg" />
+    </div>
+    <div className="content">
+        <p> Hi, My name is Logan and we are the   !</p>
+        <h1>WashCrew</h1>
+        <div>
+            <Link to="/project"
+            className="btn">Projects</Link>
+            <Link to="/contact"
+            className="btn btn-light">Contact</Link>
+        </div>
+    </div>
+</div>
+);
+};
+
+export default HeroImg;
---
+++ src/components/WorkCradData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.jpg"
+import pro2 from "../assets/project2.jpg"
+import pro3 from "../assets/project3.jpg"
+
+const projectCardData = [
+    {
+        imgsrc: pro1,
+        title: "600SQFt Job in Allen",
+        text: "skdjfsdhfserghfuikhseriukhguisefhguisehruighseiurg",
+        view: "url.com"
+    },
+    {
+        imgsrc: pro2,
+        title: "250SQFT driveway Job",
+        text: "skdjfsdhfserghfuikhseriukhguisefhguisehruighseiurg",
+        view: "url.com"
+    },
+    {
+        imgsrc: pro3,
+        title: "900SQFT Job in Allen",
+        text: "skdjfsdhfserghfuikhseriukhguisefhguisehruighseiurg",
+        view: "url.com"
+    },
+];
+
+export default projectCardData;
